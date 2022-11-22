cdd55eb42f3f81cfa46cad0f0d187850f4f58f16
Minh Trang <minhtrang.9096@gmail.com>
2022-11-22 12:08:01 +0700

    formik

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -3,6 +3,8 @@ import { LogBox, View, Text, Alert, TextInput, TouchableOpacity, Switch, Activit
 import { SIZES, COLORS, FONTS } from "../constants";
 import { AntDesign, Fontisto } from '@expo/vector-icons';
 import DatePicker from "../components/DatePicker";
+import * as yup from 'yup';
+import { Formik } from "formik";
 /* redux */
 import { useSelector } from "react-redux";
 import actions from "../redux/actions";
@@ -15,45 +17,41 @@ const Detail = ({ navigation, route }) => {
 
     const state = useSelector((state) => state.todo.state);
     const loading = useSelector((state) => state.todo.loading);
-
-    const [todo, setTodo] = React.useState(null);
     const [show, setShow] = React.useState(false);
+    const [date, setDate] = React.useState(new Date());
+
+
+    console.log("Details render");
+
+    console.log(date);
 
     React.useEffect(() => {
-        setTodo(route.params.item);
+        setDate(route.params.item.date);
     }, []);
 
-    const updateState = (key, value) => {
-        const temp = { ...todo };
-        temp[key] = value;
-        setTodo({
-            ...temp
-        });
-    }
+    const validate = yup.object().shape({
+        job: yup.string().required("Name job is required !")
+    });
 
-    const updateTodo = () => {
-        if (todo.job !== "") {
-            actions.updateTodo(todo)
-                .then((result) => {
-                    Alert.alert(result);
-                    if (state.single) {
-                        actions.checkChangeDate(state.date);
-                    }
-                    else {
-                        actions.getListFilter(state.date.start, state.date.end);
-                    }
-                })
-        }
-        else {
-            Alert.alert("Job is empty !");
-        }
+    const updateTodo = (values) => {
+        actions.updateTodo({
+            ...values,
+            date
+        })
+            .then((result) => {
+                Alert.alert(result);
+                if (state.single) {
+                    actions.checkChangeDate(state.date);
+                }
+                else {
+                    actions.getListFilter(state.date.start, state.date.end);
+                }
+            });
     }
-
-    const toggleSwitch = () => updateState("complete", !todo.complete);
 
     const checkEnable = () => {
-        if (todo?.date.getFullYear() <= today.getFullYear() && todo?.date.getMonth() <= today.getMonth()
-            && todo?.date.getDate() < today.getDate()
+        if (date.getFullYear() <= today.getFullYear() && date.getMonth() <= today.getMonth()
+            && date.getDate() < today.getDate()
         ) {
             return true;
         }
@@ -108,123 +106,152 @@ const Detail = ({ navigation, route }) => {
                     paddingHorizontal: SIZES.padding
                 }}
             >
-                {/* Name job */}
+                <Formik
+                    enableReinitialize={true}
+                    validationSchema={validate}
+                    initialValues={{
+                        ...route.params.item
+                    }}
+                    onSubmit={(values) => {
+                        validate
+                            .then((valid) => {
+                                console.log(valid);
+                                if (valid) {
+                                    updateTodo(values);
+                                }
+                                else {
+                                    Alert.alert("There is an error in your form !");
+                                }
+                            })
 
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        marginBottom: SIZES.padding
                     }}
                 >
-                    <Text style={{ ...FONTS.h3 }}>To do :</Text>
-                    <TextInput
-                        defaultValue={todo?.job}
-                        style={{
-                            borderColor: COLORS.white,
-                            marginLeft: SIZES.base,
-                            flex: 1,
-                            borderWidth: 1,
-                            borderRadius: SIZES.radius,
-                            padding: SIZES.base * 2,
-                            backgroundColor: COLORS.blue,
-                            color: COLORS.white,
-                            ...FONTS.h3
-                        }}
-                        onChangeText={(job) => updateState("job", job)}
-                    />
-                </View>
+                    {({ handleChange, touched, handleSubmit, values, setFieldValue, errors, handleBlur }) => {
+                        console.log(touched);
+                        return (
+                            <>
+                                {/* Name job */}
+                                <View
+                                    style={{
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        marginBottom: SIZES.base
+                                    }}
+                                >
+                                    <Text style={{ ...FONTS.h3 }}>To do :</Text>
+                                    <TextInput
+                                        value={values?.job}
+                                        style={{
+                                            borderColor: COLORS.white,
+                                            marginLeft: SIZES.base,
+                                            flex: 1,
+                                            borderWidth: 1,
+                                            borderRadius: SIZES.radius,
+                                            padding: SIZES.base * 2,
+                                            backgroundColor: COLORS.blue,
+                                            color: COLORS.white,
+                                            ...FONTS.h3
+                                        }}
+                                        onChangeText={handleChange('job')}
+                                        onBlur={handleBlur('job')}
+                                    />
+                                </View>
+                                {(errors.job && touched.job) &&
+                                    <Text style={{ ...FONTS.h3_light, color: "red" }}>{errors.job}</Text>
+                                }
 
-                {/* Date */}
+                                {/* Date */}
 
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        marginBottom: SIZES.padding
+                                <View
+                                    style={{
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        marginVertical: SIZES.padding
+                                    }}
+                                >
+                                    <Text style={{ ...FONTS.h3 }}>Date :</Text>
+
+                                    <View
+                                        style={{
+                                            backgroundColor: COLORS.orange,
+                                            borderColor: COLORS.white,
+                                            marginLeft: SIZES.base,
+                                            flex: 1,
+                                            borderWidth: 1,
+                                            borderRadius: SIZES.radius,
+                                            padding: SIZES.base * 2,
+                                            flexDirection: "row",
+                                            alignItems: "center",
+                                            justifyContent: "space-between"
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.h3, color: COLORS.white }}>{formatDate(date)}</Text>
+
+                                        <TouchableOpacity
+                                            style={{
+                                                height: 32,
+                                                width: 32,
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                marginLeft: SIZES.base,
+                                            }}
+
+                                            onPress={() => setShow(true)}
+                                        >
+                                            <Fontisto name="date" size={25} color={COLORS.white} />
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+
+                                <View
+                                    style={{
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        marginBottom: SIZES.padding
+                                    }}
+                                >
+                                    <Text style={{ ...FONTS.h3, marginRight: SIZES.base }}>Complete :</Text>
+                                    <Switch
+                                        trackColor={{ false: COLORS.white, true: COLORS.complete }}
+                                        thumbColor={COLORS.pink}
+                                        ios_backgroundColor={COLORS.white}
+                                        onValueChange={() => setFieldValue('complete', !values.complete)}
+                                        value={values?.complete}
+                                        disabled={checkEnable()}
+                                    />
+                                </View>
+
+                                <View
+                                    style={{
+                                        alignItems: "center",
+                                        marginTop: SIZES.padding
+                                    }}
+                                >
+                                    <TouchableOpacity
+                                        style={{
+                                            width: "90%",
+                                            height: 60,
+                                            backgroundColor: COLORS.pink,
+                                            justifyContent: "center",
+                                            alignItems: "center",
+                                            borderRadius: SIZES.radius
+                                        }}
+
+                                        onPress={handleSubmit}
+                                    >
+                                        {
+                                            loading ?
+                                                <ActivityIndicator size="large" color={COLORS.white} />
+                                                :
+                                                <Text style={{ ...FONTS.h3, color: COLORS.white }}>Update</Text>
+                                        }
+                                    </TouchableOpacity>
+                                </View>
+                            </>
+                        )
                     }}
-                >
-                    <Text style={{ ...FONTS.h3 }}>Date :</Text>
+                </Formik>
 
-                    <View
-                        style={{
-                            backgroundColor: COLORS.orange,
-                            borderColor: COLORS.white,
-                            marginLeft: SIZES.base,
-                            flex: 1,
-                            borderWidth: 1,
-                            borderRadius: SIZES.radius,
-                            padding: SIZES.base * 2,
-                            flexDirection: "row",
-                            alignItems: "center",
-                            justifyContent: "space-between"
-                        }}
-                    >
-                        <Text style={{ ...FONTS.h3, color: COLORS.white }}>{formatDate(todo?.date)}</Text>
-
-                        <TouchableOpacity
-                            style={{
-                                height: 32,
-                                width: 32,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                marginLeft: SIZES.base,
-                            }}
-
-                            onPress={() => setShow(true)}
-                        >
-                            <Fontisto name="date" size={25} color={COLORS.white} />
-                        </TouchableOpacity>
-                    </View>
-
-
-
-                </View>
-
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        marginBottom: SIZES.padding
-                    }}
-                >
-                    <Text style={{ ...FONTS.h3, marginRight: SIZES.base }}>Complete :</Text>
-                    <Switch
-                        trackColor={{ false: COLORS.white, true: COLORS.complete }}
-                        thumbColor={COLORS.pink}
-                        ios_backgroundColor={COLORS.white}
-                        onValueChange={toggleSwitch}
-                        value={todo?.complete}
-                        disabled={checkEnable()}
-                    />
-                </View>
-
-                <View
-                    style={{
-                        alignItems: "center",
-                        marginTop: SIZES.padding
-                    }}
-                >
-                    <TouchableOpacity
-                        style={{
-                            width: "90%",
-                            height: 60,
-                            backgroundColor: COLORS.pink,
-                            justifyContent: "center",
-                            alignItems: "center",
-                            borderRadius: SIZES.radius
-                        }}
-
-                        onPress={() => updateTodo()}
-                    >
-                        {
-                            loading ?
-                                <ActivityIndicator size="large" color={COLORS.white} />
-                                :
-                                <Text style={{ ...FONTS.h3, color: COLORS.white }}>Update</Text>
-                        }
-                    </TouchableOpacity>
-                </View>
             </View>
         )
     }
@@ -234,7 +261,7 @@ const Detail = ({ navigation, route }) => {
             style={{ flex: 1, backgroundColor: COLORS.white }}
             onPress={() => Keyboard.dismiss()}
         >
-            <DatePicker show={show} setShow={setShow} setChooseDate={(date) => updateState("date", date)} check={false} />
+            <DatePicker show={show} setShow={setShow} setChooseDate={setDate} check={false} />
             {renderHeader()}
             {renderContent()}
         </Pressable>
